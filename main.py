5adc6392af7414cda386a4f79785201561c9c014
bparthi25 <77563932+bparthi25@users.noreply.github.com>
2023-01-20 14:40:36 +0530

    Practical application

---
+++ main.py
@@ -0,0 +1,44 @@
+from option_1 import *
+from option_2 import *
+from option_3 import *
+from option_4 import *
+from option_5 import *
+from support import *
+
+class Menu:
+    def __init__(self, command, text, fn):
+        self.command = command
+        self.text = text
+        self.fn = fn
+
+def _exit():
+    close_db()
+    quit()
+
+menus = [
+    Menu(1, '1. Retrieve the details of the top ranked song for a particular day', option_1),
+    Menu(2, '2. Retrieve the details of the artists having top ranked songs in a particular year', option_2),
+    Menu(3, '3. Retrieve the details of the song with the longest number of weeks on the board of a particular artist', option_3),
+    Menu(4, '4. Retrieve the song that has moved the most in ranking on the board in a particular year', option_4),
+    Menu(5, '5. Visualize the top songs in a particular year', option_5),
+    Menu(6, '6. Exit', _exit)
+]
+
+#Connect to the db
+connect_db("charts.db")
+
+dic_menu = {}
+for m in menus:
+    dic_menu[m.command] = m
+
+while (True):
+    for m in menus:
+        print(m.text)
+    command = None
+    while(command == None or command > 6 or command < 1):
+        try:
+            command = int(input('Select option: '))
+        except:
+            print('Please enter a valid number')
+    if dic_menu[command]:
+        dic_menu[command].fn()
---
+++ support.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+import sqlite3
+import os.path
+
+
+db: sqlite3.Connection = None
+
+def connect_db(filename):
+    """Create a connection to the sqlite database.
+
+    Parameters
+    
+    filename: string
+        name of database file
+
+    Returns
+    
+    Connection
+        a connection to the database
+    """
+    # Absolute path for database file
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_path =  os.path.join(BASE_DIR, filename)
+
+    global db
+    if not db:
+        db = sqlite3.connect(file_path)
+    return db
+
+
+def close_db():
+    """Close the connection to the sqlite database.
+    """
+    global db
+    if db:
+        db.close()
+        db = None
+
+
+def query(sql_query, param):
+    """Execute sql query
+
+    Parameters
+  
+    sql_query : string
+        A valid sql query
+    param : any
+        A parameter to be passed in sql query
+
+    Returns
+    
+    list
+        a list of rows returned from the database by this query
+    """
+    # Execute sql
+    cur = db.cursor()
+    cur.execute(sql_query, (param,))
+    rows = cur.fetchall()
+    return rows
+
+# Ensure that user enter a valid date
+
+
+def input_date():
+    """
+    Require user to input a date until it is valid
+
+    Returns
+    
+    datetime
+        a valid date
+    """
+    date = None
+    while (date == None):
+        try:
+            date_str = input('Date to look up (yyyy-mm-dd): ')
+            date_list = date_str.split('-')
+            date = datetime(int(date_list[0]), int(
+                date_list[1]), int(date_list[2]))
+        except:
+            print("Please enter a valid date")
+    return date
+
+
+def input_year():
+    """
+    Require user to input a year until it is valid
+
+    Returns
+    
+    int
+        a valid year
+    """
+    year = None
+    while (year == None):
+        try:
+            year = int(input('Year to look up (yyyy): '))
+        except:
+            print("Please enter a valid year")
+    return year
+
+
+def input_str(key):
+    """
+    Require user to input a string until it is not empty
+
+    Parameters
+    
+    key: string
+        {key} to look up: 
+
+    Returns
+    
+    string
+        a not empty string
+    """
+    str = ''
+    while (str == ''):
+        try:
+            str = input(f'{key} to look up: ')
+        except:
+            print(f"Please enter a valid {key}")
+    return str
